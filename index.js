7cbdccd4cad9adeaf0e38e836128766c425bac66
Gilbert Ardila <gilbertferney@gmail.com>
2022-02-25 15:29:04 -0500

    agregamos media queries, finalizamos la sección de contacto y experiencia laboral

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 
 var menu=document.getElementById("nav");
 var styles=getComputedStyle(menu);
+var enviar=document.getElementById('enviar');
 
+
+
+/*  Función para mostrar el desplaegable del menú y/o ocultarlo 
+con un click*/
 $('#menu_img').click(function(){
 
    
